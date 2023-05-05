a7c029de2de065afec64c7282fe0356f403200fd
eriieii <eriiferdian92@gmail.com>
2023-05-05 14:19:38 +0700

    fix config

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/http://127.0.0.1:8000/api/',
+    baseURL: 'http://127.0.0.1:8000/api/',
   },
   
   auth: {
@@ -67,9 +67,9 @@ export default {
           login: { url: '/login', method: 'post' },
           logout: { url: '/logout', method: 'post' },
           user: { url: '/user', method: 'get' }
-        }
-      }
-    }
+        },
+      },
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
